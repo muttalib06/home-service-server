a9371e5605b3d57e3e3dd32837061924a17b11f3
Md. Muttalib <mdabdulmottalib2222@gmail.com>
2025-11-13 17:06:10 +0600

    added delete api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -74,6 +74,14 @@ async function run() {
       const result = await serviceCollection.insertOne(service);
       res.send(result);
     });
+
+    // Delete API;
+    app.delete("/myServices/:id",async(req,res) => {
+      const id = req.params.id;
+      const query = {_id:new ObjectId(id)};
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result)
+    })
 
     // send a ping to successful connection;
     await client.db("admin").command({ ping: 1 });
