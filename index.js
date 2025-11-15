83f626fc6a23a5e95a8af0f29d9764ef6abd72cc
Md. Muttalib <mdabdulmottalib2222@gmail.com>
2025-11-15 12:25:25 +0600

    added code correction

--- index.js
+++ index.js
@@ -143,7 +143,7 @@ async function run() {
     });
 
     // send a ping to successful connection;
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment.You successfully connected to the MongoDB"
     );
